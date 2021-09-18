e0a18dc550e8b5522276d76de7055fc982d0c939
aliassaad66 <alias_saad66@yahoo.com>
2021-09-18 13:15:52 +0800

    Finished chat app

---
+++ src/utils/supabase.js
@@ -0,0 +1,12 @@
+const React = require("react");
+
+const { createClient } = require("@supabase/supabase-js");
+
+const supabaseURL = process.env.SUPABASE_API_URL;
+const supabaseApiKey = process.env.SUPABASE_API_KEY;
+
+const supabase = createClient(supabaseURL, supabaseApiKey);
+
+module.exports = supabase;
+
+
